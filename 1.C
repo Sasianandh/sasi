fc68a0ec0a6eabae6308660975163c99696cbee5
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2017-09-05 16:17:14 +0530

    Update and rename odd.c to 1.C

--- 1.C
+++ 1.C
@@ -1,12 +1,13 @@
 #include <stdio.h>
-int main()
+main()
 {
 int num;
-printf("Enter an integer: ");
+printf("Enter an int: ");
     scanf("%d", &num);
-    if(number % 2 == 0)
+    if(num % 2 == 0)
         printf("%d is even.", num);
         else
         printf("%d is odd.", num);
 return 0;
 }
+
