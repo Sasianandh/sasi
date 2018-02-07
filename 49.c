a6d5c9143f547322f490c5ceaa7daa593b104cb5
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-02-07 16:58:59 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the number\n");
+scanf("%d",&a);
+printf("enter the 2nd number\n");
+scanf("%d",&b);
+c=a+b;
+printf("sum is\n");
+printf("%d",c);
+}
+
