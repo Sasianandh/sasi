1469a479e9f271e4c32bce05abaa6b22f73ff9db
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-02-12 16:31:20 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+int swap (int *,int *);
+int main()
+{
+int i,n,j,a;
+printf("enter two numbers n & a");
+scanf("%d %d",&n,&a);
+printf("after swap values are n=%d a=%d",n,a);
+swap(&n,&a);
+printf("after swap values are n=%d a=%d",n,a);
+}
+int swap(int *x,int *y)
+{
+    int t;
+    t=*x;
+    *x=*y;
+    *y=t;
+    }
