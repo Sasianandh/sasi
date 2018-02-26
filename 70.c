44ded0511bb5c43c48ffd477a385dcdc2375c032
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-02-26 16:46:10 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a=1,c,i;
+    printf("enter the num");
+   scanf("%d",&n);
+   c=n/2;
+    for(i=1;i<=c+1;i++)
+    {
+        if(a<=n)
+        a=a*2;
+        else
+        break;
+    }
+    printf("%d's nearest greater power of 2 is = %d",n,a);
+}
+
+
