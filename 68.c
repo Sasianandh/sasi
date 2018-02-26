3fab51bc2cd4a45e79b03d24d1a9afbe95f1b0fc
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-02-26 16:00:34 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,b,c;
+    printf("enter the num");
+   scanf("%d",&n);
+    if(n%7==0)
+       printf("yes");
+       else
+       printf("no");
+   }
