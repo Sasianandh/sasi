f33eea6fc0b4f297de4c4a4e0addb1599dc6ac70
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-02-26 15:57:33 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,b,c;
+    printf("enter the num");
+   scanf("%d",&n);
+    b=n/10;
+       printf("nearby greatest num is %d",(b+1)*10);
+   }
