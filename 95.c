436ffa99d43d651c8870fe8c1adf2eb61a35522f
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-11 21:20:28 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int p,r,t,a;
+    printf("enter the amount");
+    scanf("%d",&p);
+     printf("enter the rate");
+     scanf("%d",&r);
+      printf("enter the time");
+      scanf("%d",&t);
+      a=p*(r*t)/100;
+printf("value = %d",a);
+}
