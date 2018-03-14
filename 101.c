4e7d76f7063715545e952c1d3d3844c118b432cb
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-14 16:59:55 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+       char s[100];
+       gets(s);
+       int i,c,d;
+       scanf("%d",&d);
+       c=strlen(s);
+       for(i=c-1;i>c-1-d;i--)
+       {
+               printf("%c",s[i]);
+       }
+}
