29881eb8d1d00a3a9acf6ef809e6d26b757969d0
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-15 20:54:12 +0530

    Update 101.c

--- 101.c
+++ 101.c
@@ -2,8 +2,10 @@
 void main()
 {
        char s[100];
+       printf("enter string");
        gets(s);
        int i,c,d;
+       printf("enter position");
        scanf("%d",&d);
        c=strlen(s);
        for(i=c-1;i>c-1-d;i--)
